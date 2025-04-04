ffb13a9314f61bb3541be4cb20e798370e98e3f8
nora bockert <nbockert@bu.edu>
2025-04-04 12:26:53 -0400

    Bug fix 3

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -2,9 +2,8 @@
 
 import RecipeDetailPage from "@/app/recipes/RecipeDetail";
 
-export default async function Page({ searchParams }: { searchParams: { id: string } }) {
-    const param = await searchParams
-    const id = await param.id
+export default async function Page(props: { searchParams: Promise<{ id: string }>} ) {
+    const { id } = await props.searchParams;
     return <RecipeDetailPage id={id} />;
 }
 
